131f017fb02f4dd0ee80a5199d150d50b82818f1
Joshua Jung <wbqdinno@gmail.com>
2014-06-25 04:30:51 +0900

    add font awesome

--- wordpress/wp-content/themes/fruitful/header.php
+++ wordpress/wp-content/themes/fruitful/header.php
@@ -1,10 +1,10 @@
 <?php
-/** 
-* The Header for our theme. 
-* Displays all of the <head> section and everything up till <div id="main"> 
-* @package WordPress 
-* @subpackage Fruitful theme 
-* @since Fruitful theme 1.0 
+/**
+* The Header for our theme.
+* Displays all of the <head> section and everything up till <div id="main">
+* @package WordPress
+* @subpackage Fruitful theme
+* @since Fruitful theme 1.0
 **/
 ?><!DOCTYPE html>
 <!--[if IE 7]><html class="ie ie7" <?php language_attributes(); ?>><![endif]-->
@@ -21,68 +21,69 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php fruitful_get_favicon(); ?>
 <!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script><![endif]-->
-<?php wp_head(); ?> 
+<?php wp_head(); ?>
+<link id="font-awesome" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" media="all">
 </head>
-<body <?php 
+<body <?php
 		$additional_body_classes = '';
 		if (class_exists('Woocommerce')) {
 			if (is_shop()) { $additional_body_classes .= 'shop-page '; }
-		} 
+		}
 		$theme_options = fruitful_ret_options("fruitful_theme_options");
 		if (isset($theme_options['responsive']) && ($theme_options['responsive'] == 'on')) {
 			$additional_body_classes .= 'responsive ';
 		}
-		body_class(trim($additional_body_classes)); 
+		body_class(trim($additional_body_classes));
 	  ?>>
 	<div id="page" class="hfeed site">
-		<div class="head-container">		
-			<div class="container">			
-				<div class="sixteen columns">				
+		<div class="head-container">
+			<div class="container">
+				<div class="sixteen columns">
 					<header id="masthead" class="site-header" role="banner">
-						<?php	
-							if (fruitful_is_social_header()) { 
-								fruitful_get_socials_icon(); 
-							} 
-									
+						<?php
+							if (fruitful_is_social_header()) {
+								fruitful_get_socials_icon();
+							}
+
 							$logo_pos_class = $menu_pos_class = '';
 							$options = fruitful_get_theme_options();
 							$logo_position = $options['logo_position'];
 							$menu_position = $options['menu_position'];
-							
+
 							$logo_pos_class = fruitful_get_class_pos($logo_position);
 							$menu_pos_class = fruitful_get_class_pos($menu_position);
 						?>
-						
-						<div data-originalstyle="<?php echo $logo_pos_class; ?>" class="header-hgroup <?php echo $logo_pos_class; ?>">  
+
+						<div data-originalstyle="<?php echo $logo_pos_class; ?>" class="header-hgroup <?php echo $logo_pos_class; ?>">
 							<?php echo fruitful_get_logo(); ?>
-						</div>	
-							
+						</div>
+
 						<div data-originalstyle="<?php echo $menu_pos_class; ?>" class="menu-wrapper <?php echo $menu_pos_class; ?>">
 							<?php fruitful_get_languages_list(); ?>
-								
+
 							<?php if (class_exists('Woocommerce')) { ?>
 								<?php if (!empty($theme_options['showcart'])) {
 										if (($theme_options['showcart']) == 'on'){?>
 											<div class="cart-button">
 												<a href="<?php echo get_permalink( woocommerce_get_page_id( 'cart' ) ); ?>" class="cart-contents">
-													<div class="cart_image"></div> 
+													<div class="cart_image"></div>
 													<span class="num_of_product_cart"><?php global $woocommerce;
 													echo $woocommerce->cart->cart_contents_count; ?> </span>
 												</a>
-											</div>							
+											</div>
 									<?php } ?>
 								<?php } ?>
 							<?php } ?>
-								
+
 							<nav role="navigation" class="site-navigation main-navigation">
-								<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>					
-							</nav><!-- .site-navigation .main-navigation -->	
+								<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+							</nav><!-- .site-navigation .main-navigation -->
 						</div>
-					</header><!-- #masthead .site-header -->			
-				</div>		
-			</div>	
-		</div>		
-		
-		<div class="container page-container">		
-			<?php do_action( 'before' ); ?>		
+					</header><!-- #masthead .site-header -->
+				</div>
+			</div>
+		</div>
+
+		<div class="container page-container">
+			<?php do_action( 'before' ); ?>
 				<div class="sixteen columns">
