5a8b706a0cc0c14a37d51caf3ffb3ee9607b94da
Joshua Jung <wbqdinno@gmail.com>
2014-07-01 06:44:43 +0900

    add breadcrumb

--- wordpress/wp-content/themes/fruitful/page.php
+++ wordpress/wp-content/themes/fruitful/page.php
@@ -26,6 +26,9 @@ get_header(); ?>
 			<div id="primary" class="content-area">
 				<div id="content" class="site-content" role="main">
 				<?php while ( have_posts() ) : the_post(); ?>
+					<?php if ( function_exists('yoast_breadcrumb') ) {
+						yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+						} ?>
 					<?php get_template_part( 'content', 'page' ); ?>
 					<?php if (fruitful_state_page_comment()) { comments_template( '', true );  }	 ?>
 				<?php endwhile; // end of the loop. ?>
