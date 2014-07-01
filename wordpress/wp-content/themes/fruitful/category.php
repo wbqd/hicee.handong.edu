8a806df4bdf1276424c9e5219b629470ef65cabe
Joshua Jung <wbqdinno@gmail.com>
2014-07-01 22:23:25 +0900

    Delete showing 'Category Archives' in category.php

--- wordpress/wp-content/themes/fruitful/category.php
+++ wordpress/wp-content/themes/fruitful/category.php
@@ -8,7 +8,6 @@
 get_header(); ?>
 
 <header class="archive-header">
-	<h1 class="archive-title"><?php printf( __( '%s', 'fruitful' ), single_cat_title( '', false ) ); ?></h1>
 	<?php if ( category_description() ) : // Show an optional category description ?>
 		<div class="archive-meta"><?php echo category_description(); ?></div>
 	<?php endif; ?>
