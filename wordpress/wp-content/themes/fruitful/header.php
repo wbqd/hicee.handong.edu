79ba00082b25dfe7afecff177bfe6a214d72a981
Joshua Jung <wbqdinno@gmail.com>
2014-07-22 14:10:22 +0900

    Add ie-edge

--- wordpress/wp-content/themes/fruitful/header.php
+++ wordpress/wp-content/themes/fruitful/header.php
@@ -15,6 +15,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
 <?php fruitful_metadevice(); ?>
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
